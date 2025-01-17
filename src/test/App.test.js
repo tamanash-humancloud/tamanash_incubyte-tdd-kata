91bac2010fbd5586710e8181b776a02edd8ad008
tamanash-humancloud <tamanash.malkhandi@humancloud.co.in>
2025-01-17 10:21:19 +0530

    written test case for handling multiplication logic correctly

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -56,4 +56,17 @@ describe('Calculator Component', () => {
             expect(display).toHaveTextContent('5'); 
         })
     });
+
+    it('should handle multiplication correctly', async () => {
+        render(<Calculator />);
+        userEvent.click(screen.getByRole('button', { name: '4' }));
+        userEvent.click(screen.getByRole('button', { name: '*' }));
+        userEvent.click(screen.getByRole('button', { name: '2' }));
+        userEvent.click(screen.getByRole('button', { name: '=' }));
+
+        await waitFor(() => {
+            const display = screen.getByRole('display'); 
+            expect(display).toHaveTextContent('8'); 
+        })
+    });
 })
